56911ab44c13aca486b6ed55b1f0629c1e11ced6
vovai <volodymyr.ihnatov@student.tuke.sk>
2026-02-20 11:32:33 +0100

    Test commit

---
+++ src/main/java/com/example/physicssimulator/PhysicsSimulatorApplication.java
@@ -0,0 +1,14 @@
+package com.example.physicssimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicsSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhysicsSimulatorApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/physicssimulator/PhysicsSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.physicssimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicsSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
