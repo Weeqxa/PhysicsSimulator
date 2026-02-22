ad60114e64bf3c753b7236639d66cf6eb1e1c85c
vovai <volodymyr.ihnatov@student.tuke.sk>
2026-02-22 18:05:15 +0100

    feat(auth): add JwtService for generating JWT tokens

---
+++ backend/src/main/java/com/example/physicssimulator/service/JwtService.java
@@ -0,0 +1,46 @@
+package com.example.physicssimulator.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY =
+            "mysecretkeymysecretkeymysecretkeymysecretkey"; // мін 256 біт
+
+    private Key getSignKey() {
+        byte[] keyBytes = SECRET_KEY.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token, String username) {
+        return extractUsername(token).equals(username);
+    }
+
+}
