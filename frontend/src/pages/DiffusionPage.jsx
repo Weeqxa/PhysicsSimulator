d303f64161669a934b26e59c3514b66e66b1091d
vovai <volodymyr.ihnatov@student.tuke.sk>
2026-03-01 20:45:31 +0100

    feat(diffusion-page): add top bar and embed DiffusionSimulation

---
+++ frontend/src/pages/DiffusionPage.jsx
@@ -0,0 +1,21 @@
+// src/pages/diffusion/DiffusionPage.jsx
+import "../styles/Common.css";
+import styles from "../styles/Diffusion.module.css";
+import DiffusionSimulation from "../components/diffusion/DiffusionSimulation.jsx";
+
+export default function DiffusionPage() {
+    return (
+        <div>
+            <div className="top-bar">
+                <div className="logo">Physical Simulations</div>
+                <div className="top-right-buttons">
+                    <a href="/" className="btn">Home</a>
+                </div>
+            </div>
+
+            <div className={styles.simulationContainer}>
+                <DiffusionSimulation />
+            </div>
+        </div>
+    );
+}
