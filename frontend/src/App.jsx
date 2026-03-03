6928e3bc09b035422e89408f5678939e423718b3
vovai <volodymyr.ihnatov@student.tuke.sk>
2026-03-03 21:40:42 +0100

    new page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import PendulumPage from "./pages/PendulumPage";
-import DiffusionPage from "./pages/DiffusionPage"; // <-- імпорт DiffusionPage
+import DiffusionPage from "./pages/DiffusionPage";
+import SpringPage from "./pages/SpringPage";
 
 function App() {
     return (
@@ -14,7 +15,8 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/profile" element={<div>Profile Page</div>} />
                 <Route path="/pendulum" element={<PendulumPage />} />
-                <Route path="/diffusion" element={<DiffusionPage />} /> {/* <-- новий маршрут */}
+                <Route path="/diffusion" element={<DiffusionPage />} />
+                <Route path="/spring" element={<SpringPage />} />
             </Routes>
         </Router>
     );
