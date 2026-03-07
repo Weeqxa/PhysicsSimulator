ced5314212db7d9dc15ee1e9406bcd35d32af579
vovai <volodymyr.ihnatov@student.tuke.sk>
2026-03-07 18:41:24 +0100

    feat: create Magnet page and integrate simulation component

---
+++ frontend/src/pages/MagnetPage.jsx
@@ -0,0 +1,24 @@
+import "../styles/Common.css";
+import styles from "../styles/Magnet.module.css";
+import MagnetSimulation from "../components/magnet/MagnetSimulation";
+
+export default function MagnetPage() {
+    return (
+        <div>
+            <div className="top-bar">
+                <div className="logo">Physical Simulations</div>
+                <div className="top-right-buttons">
+                    <a href="/" className="btn">Home</a>
+                </div>
+            </div>
+
+            <div className={styles["simulation-container"]}>
+                <div className={styles["simulation-panel"]}>
+                    <div className={styles.container}>
+                        <MagnetSimulation />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
