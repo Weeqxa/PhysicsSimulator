87a28df30b48f7f5bc3b07c742965b2accb648d8
vovai <volodymyr.ihnatov@student.tuke.sk>
2026-03-12 15:38:20 +0100

    new page for buoyancy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -6,19 +6,22 @@ import PendulumPage from "./pages/PendulumPage";
 import DiffusionPage from "./pages/DiffusionPage";
 import SpringPage from "./pages/SpringPage";
 import MagnetPage from "./pages/MagnetPage";
+import BuoyancyPage from "./pages/BuoyancyPage";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/profile" element={<div>Profile Page</div>} />
-                <Route path="/pendulum" element={<PendulumPage />} />
-                <Route path="/diffusion" element={<DiffusionPage />} />
-                <Route path="/spring" element={<SpringPage />} />
-                <Route path="/magnet" element={<MagnetPage />} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/profile" element={<div>Profile Page</div>}/>
+                <Route path="/pendulum" element={<PendulumPage/>}/>
+                <Route path="/diffusion" element={<DiffusionPage/>}/>
+                <Route path="/spring" element={<SpringPage/>}/>
+                <Route path="/magnet" element={<MagnetPage/>}/>
+                <Route path="/buoyancy" element={<BuoyancyPage/>}/>
+
             </Routes>
         </Router>
     );
